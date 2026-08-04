59be1aad1832c040328a1d4ebe1c3cbf92a585da
Jeremy Howard <github@jhoward.fastmail.fm>
2026-08-04 21:08:43 +1000

    fixes #4

--- jupyasyncclient/__init__.py
+++ jupyasyncclient/__init__.py
@@ -1,7 +1,14 @@
+"""Async kernel client for Jupyter Server via HTTP/WebSocket
+
+Modules:
+
+- `jupyasyncclient.term`: A client for gateway-hosted terminals: REST lifecycle plus one websocket attachment"""
+
 __version__ = "0.2.1"
 
-from .core import JupyAsyncKernelClient, dumps, loads, serialize_binary_message, deserialize_binary_message
+from .core import JupyAsyncKernelClient, Run, DeadKernelError, dumps, loads, serialize_binary_message, deserialize_binary_message
 from .manager import JupyAsyncKernelManager, start_new_server_kernel
 from .multimanager import JupyAsyncMultiKernelManager
+from .term import JupyAsyncTerminalClient
 
-__all__ = ["JupyAsyncKernelClient", "JupyAsyncKernelManager", "JupyAsyncMultiKernelManager", "start_new_server_kernel"]
+__all__ = ["JupyAsyncKernelClient", "Run", "DeadKernelError", "JupyAsyncKernelManager", "JupyAsyncMultiKernelManager", "JupyAsyncTerminalClient", "start_new_server_kernel"]
