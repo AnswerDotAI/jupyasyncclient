a153685df0a954dd99c1cbd00336f6131a855da6
Jeremy Howard <github@jhoward.fastmail.fm>
2026-08-04 21:10:18 +1000

    bump

--- jupyasyncclient/__init__.py
+++ jupyasyncclient/__init__.py
@@ -4,7 +4,7 @@ Modules:
 
 - `jupyasyncclient.term`: A client for gateway-hosted terminals: REST lifecycle plus one websocket attachment"""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .core import JupyAsyncKernelClient, Run, DeadKernelError, dumps, loads, serialize_binary_message, deserialize_binary_message
 from .manager import JupyAsyncKernelManager, start_new_server_kernel
